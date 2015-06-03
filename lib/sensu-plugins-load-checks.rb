6ef4e5dba9ff7a00ee9d3453a172349b1e655569
Matt Jones <mattjones@yieldbot.com>
2015-06-03 11:50:29 -0400

    deploy

--- lib/sensu-plugins-load-checks.rb
+++ lib/sensu-plugins-load-checks.rb
@@ -1,14 +1,1 @@
 require 'sensu-plugins-load-checks/version'
-
-# Load the defaults
-
-#
-# Default class
-#
-module SensuPluginsLoadChecks
-  class << self
-  end
-
-  class << self
-  end
-end
--- lib/sensu-plugins-load-checks/version.rb
+++ lib/sensu-plugins-load-checks/version.rb
@@ -1,28 +1,7 @@
-require 'json'
-
-# encoding: utf-8
 module SensuPluginsLoadChecks
-  # This defines the version of the gem
   module Version
     MAJOR = 0
     MINOR = 0
-    PATCH = 1
-
-    VER_STRING = [MAJOR, MINOR, PATCH].compact.join('.')
-
-    NAME   = 'sensu-plugins-load-checks'
-    BANNER = "#{NAME} v%s"
-
-    module_function
-
-    def version
-      format(BANNER, VER_STRING)
-    end
-
-    def json_version
-      {
-        'version' => VER_STRING
-      }.to_json
-    end
+    PATCH = 2
   end
 end
