fdc1d17dedd9af2cf2c0e14db5a3f296e88c664c
Ben Abrams <me@benabrams.it>
2016-09-18 11:35:37 -0700

    closes #6
    While this change is not a major code change it is a major change in default behavior. This attempts to have more 'sane' defaults. For a more in depth explanation on the rationale please see: https://github.com/sensu-plugins/sensu-plugins-load-checks/issues/6#issuecomment-239488412

--- bin/check-load.rb
+++ bin/check-load.rb
@@ -14,7 +14,7 @@
 #   gem: sensu-plugin
 #
 # USAGE:
-#
+#   ./bin/check-load.rb --help
 # NOTES:
 #
 # LICENSE:
@@ -26,7 +26,7 @@
 require 'sensu-plugin/check/cli'
 
 class LoadAverage
-  def initialize(per_core = false)
+  def initialize(per_core = true)
     @cores = per_core ? cpu_count : 1
     @avg = File.read('/proc/loadavg').split.take(3).map { |a| (a.to_f / @cores).round(2) } rescue nil # rubocop:disable RescueModifier
   end
@@ -42,7 +42,7 @@ class LoadAverage
   end
 
   def exceed?(thresholds)
-    @avg.zip(thresholds).any? { |a, t| a > t }
+    @avg.zip(thresholds).any? { |a, t| a >= t }
   end
 
   def to_s
@@ -56,25 +56,25 @@ class CheckLoad < Sensu::Plugin::Check::CLI
          long: '--warn L1,L5,L15',
          description: 'Load WARNING threshold, 1/5/15 min average',
          proc: proc { |a| a.split(',').map(&:to_f) },
-         default: [10, 20, 30]
+         default: [2.75, 2.5, 2.0]
 
   option :crit,
          short: '-c L1,L5,L15',
          long: '--crit L1,L5,L15',
          description: 'Load CRITICAL threshold, 1/5/15 min average',
          proc: proc { |a| a.split(',').map(&:to_f) },
-         default: [25, 50, 75]
+         default: [3.5, 3.25, 3.0]
 
   option :per_core,
          short: '-p',
          long: '--per-core',
          description: 'Divide load average results by cpu/core count',
          boolean: 'true',
-         default: false
+         default: true
 
   def run
     avg = LoadAverage.new(config[:per_core])
-    warning 'Could not read load average from /proc' if avg.failed?
+    unknown 'Could not read load average from /proc' if avg.failed?
     message "Load average: #{avg}"
     critical if avg.exceed?(config[:crit])
     warning if avg.exceed?(config[:warn])
--- lib/sensu-plugins-load-checks/version.rb
+++ lib/sensu-plugins-load-checks/version.rb
@@ -1,6 +1,6 @@
 module SensuPluginsLoadChecks
   module Version
-    MAJOR = 1
+    MAJOR = 2
     MINOR = 0
     PATCH = 0
 
